241400b9ffec0a25ba5f7e87e3465d9c7f2cbc3e
Allaa Sameh <louka.fawzy.96@gmail.com>
2020-01-05 03:31:05 -0800

    GetHomeBrands1

--- app/src/main/java/com/example/laptopshope/LaptopHomeActivity.java
+++ app/src/main/java/com/example/laptopshope/LaptopHomeActivity.java
@@ -1,14 +1,61 @@
 package com.example.laptopshope;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
 public class LaptopHomeActivity extends AppCompatActivity {
+
+    RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_laptop_home);
+
+        recyclerView = findViewById(R.id.recyclerView);
+    }
+
+    void GrtHomeBrands ()
+    {
+        StringRequest task = new StringRequest(Request.Method.POST, VollyData.GetHomebrands, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+
+                try {
+                    JSONObject object = new JSONObject(response);
+                    int status = object.getInt("status");
+                    if(status ==0)
+                    {
+                        JSONArray array = object.getJSONArray("data");
+                        ArrayList<laptop> laptops = new ArrayList<>();
+                        for (int i = 0 ; i<laptops.size();i++)
+                        {
+
+                        }
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        });
     }
 }
--- app/src/main/java/com/example/laptopshope/laptopAdapter.java
+++ app/src/main/java/com/example/laptopshope/laptopAdapter.java
@@ -29,7 +29,7 @@ public class laptopAdapter extends RecyclerView.Adapter<laptopAdapter.Holderlapt
     @NonNull
     @Override
     public Holderlaptop onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(context).inflate(R.layout.row_laptop,parent,false);
+        View view = LayoutInflater.from(context).inflate(R.layout.row_laptopgethome,parent,false);
         Holderlaptop holderlaptop = new Holderlaptop(view);
         return holderlaptop;
     }
@@ -38,21 +38,8 @@ public class laptopAdapter extends RecyclerView.Adapter<laptopAdapter.Holderlapt
     public void onBindViewHolder(@NonNull Holderlaptop holder, int position) {
 
        holder.tx_brand.setText(laptops.get(position).getBrand());
-       holder.tx_price.setText(laptops.get(position).getPrice() + "");
        holder.rate.setRating(laptops.get(position).getRate());
        Picasso.get().load(laptops.get(position).getImg()).into(holder.img);
-       holder.btn_back.setOnClickListener(new View.OnClickListener() {
-           @Override
-           public void onClick(View v) {
-
-           }
-       });
-       holder.btn_order.setOnClickListener(new View.OnClickListener() {
-           @Override
-           public void onClick(View v) {
-
-           }
-       });
 
     }
 
@@ -63,21 +50,16 @@ public class laptopAdapter extends RecyclerView.Adapter<laptopAdapter.Holderlapt
 
     public class Holderlaptop extends RecyclerView.ViewHolder{
 
-      TextView tx_brand,tx_price;
+      TextView tx_brand;
       RatingBar rate;
       ImageView img;
-      Button btn_back,btn_order;
-        public Holderlaptop(@NonNull View itemView) {
+
+      public Holderlaptop(@NonNull View itemView) {
             super(itemView);
 
-            tx_brand = itemView.findViewById(R.id.tx_brand);
-            tx_price = itemView.findViewById(R.id.tx_price);
-             rate = itemView.findViewById(R.id.Rate);
-             img = itemView.findViewById(R.id.img);
-             btn_back = itemView.findViewById(R.id.btn_back);
-             btn_order = itemView.findViewById(R.id.btn_order);
-
-
+            tx_brand = itemView.findViewById(R.id.tx_brandhome);
+            rate = itemView.findViewById(R.id.rate_home);
+            img = itemView.findViewById(R.id.img_home);
         }
     }
 }
